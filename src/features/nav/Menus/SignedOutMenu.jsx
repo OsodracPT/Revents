2fef42ac5cd13d00d3df260aa2ea9e76d1f5d3b0
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-03-22 23:39:20 +0000

    Adding Signed in and Signed out menus to the nav bar

---
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -0,0 +1,18 @@
+import { Menu, Button } from "semantic-ui-react";
+import React from "react";
+
+const SignedOutMenu = ({ signIn }) => {
+  return (
+    <Menu.Item position="right">
+      <Button onClick={signIn} basic inverted content="Login" />
+      <Button
+        basic
+        inverted
+        content="Sign Out"
+        style={{ marginLeft: "0.5em" }}
+      />
+    </Menu.Item>
+  );
+};
+
+export default SignedOutMenu;
