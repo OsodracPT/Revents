153b99da051f6fd24d8c05c73a83f9f0e10bfec2
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-03-23 14:54:01 +0000

    Connecting the store to our application

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./app/layouts/App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
+
+const store = configureStore();
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 };
