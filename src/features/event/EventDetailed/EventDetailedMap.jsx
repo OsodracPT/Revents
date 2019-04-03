487da7e82d27266d193cd7a0a7fe9c687041b3f5
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-04-03 23:35:57 +0100

    Adding a Map component to our application

---
+++ src/features/event/EventDetailed/EventDetailedMap.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Segment, Icon } from "semantic-ui-react";
+import GoogleMapReact from "google-map-react";
+
+const Marker = () => <Icon name="marker" size="big" color="red" />;
+
+const EventDetailedMap = ({ lat, lng }) => {
+  const center = [lat, lng];
+  const zoom = 14;
+  return (
+    <Segment attached="bottom" style={{ padding: 0 }}>
+      <div style={{ height: "300px", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyDzHlmk2FUNnJw8e82WyqwOzp3thD4HQfM"
+          }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <Marker lat={lat} lng={lng} />
+        </GoogleMapReact>
+      </div>
+    </Segment>
+  );
+};
+
+export default EventDetailedMap;
