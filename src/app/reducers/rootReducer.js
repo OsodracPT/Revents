e8c8d72b4af73fdb5be3f27284085656ab5bd56f
Pedro Cardoso <pe278@cam.ac.uk>
2019-07-30 15:00:14 +0100

    Redux thunk 101 part 1

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,16 +1,22 @@
-import { combineReducers } from "redux";
+import {
+  combineReducers
+} from "redux";
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
-import { reducer as FormReducer } from "redux-form";
+import {
+  reducer as FormReducer
+} from "redux-form";
 import modalsReducer from "../../features/modals/modalReducer";
 import authReducer from "../../features/auth/authReducer";
+import asyncReducer from "../../features/async/asyncReducer";
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
   modals: modalsReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 });
 
 export default rootReducer;
